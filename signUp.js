7c1ba382a841c0373fe26f9bae934660f1d5ab48
Mubarak150 <mkmubarak2347@gmail.com>
2023-09-26 00:23:00 -0700

    sign up form js updated

--- signUp.js
+++ signUp.js
@@ -4,46 +4,59 @@ $(document).ready(function(){
   var alert = ""; 
   // var successNote = $("#h1").next();
   
-  var password = $("#password").val(); 
-  var confirmPassword = $("#confirm-password").val();
-  var firstName = "";
-  var lastName = ""; //$(".last-name").val();
-  var fatherName = ""; //$(".father-name").val();
-  var cnic =  $("#cnic").val();
-  var contact =  $("#contact").val();
-  var email = ""; // $("#emailAddr").val(); 
- // updatesignUpButton(firstName, lastName, fatherName, cnic, contact, email, password, confirmPassword, matchPassword, lengthPassword, validateEmail, validateContact, validateCNIC)
-  
+  var passwordBox = $("#password"); 
+  var confirmPasswordBox = $("#confirm-password");
+  var firstNameBox = $(".first-name");
+  var lastNameBox = $(".last-name"); //.val();
+  var fatherNameBox = $(".father-name"); //.val();
+  var cnicBox =  $("#cnic");
+  var contactBox =  $("#contact");
+  var emailBox = $("#emailAddr"); // .val(); 
 
+  var passwordBool = false; 
+  var confirmPasswordBool = false; 
+  var cnicBool = false;
+  var contactBool = false; 
+  var emailBool = false; 
+  var firstNameBool = false;
+  var lastNameBool = false; 
+  var fatherNameBool = false; 
+ // updatesignUpButton(firstName, lastName, fatherName, cnic, contact, email, password, confirmPassword, matchPassword, lengthPassword, validateEmail, validateContact, validateCNIC)
+ 
+  var inputBool ;
   $(".name").on("input", function () {
     var inputValue = $(this).val();
     if ($(this).hasClass("first-name")) {
       firstName = inputValue;
+      firstNameBool = inputBool; 
     } else if ($(this).hasClass("last-name")) {
       lastName = inputValue;
+      lastNameBool = inputBool; 
     } else if ($(this).hasClass("father-name")) {
       fatherName = inputValue;
+      fatherNameBool = inputBool;
     }
-    validateName(inputValue, $(this), $(this).next());
+    validateName(inputValue, $(this), $(this).next(), inputBool);
     // updatesignUpButton(firstName, lastName, fatherName, cnic, contact, email, password, confirmPassword, matchPassword, lengthPassword, validateEmail, validateContact, validateCNIC)
     
   });
 
-  $("#emailAddr").on("input", function(){
-    mail = $(this).val();
-    validateEmail(mail);
+  $("#emailAddr").on("input", function(){ 
+    mail = $(this).val(); 
+    validateEmail(mail, emailBool);
     // updatesignUpButton(firstName, lastName, fatherName, cnic, contact, email, password, confirmPassword, matchPassword, lengthPassword, validateEmail, validateContact, validateCNIC)
   })
 
   $("#cnic").on("input", function(){
-    cnic = $(this).val();
-    validateCNIC(cnic);
+    cnic = $(this).val(); 
+    validateCNIC(cnic, cnicBool );
+
     // updatesignUpButton(firstName, lastName, fatherName, cnic, contact, email, password, confirmPassword, matchPassword, lengthPassword, validateEmail, validateContact, validateCNIC)
   })
 
   $("#contact").on("input", function(){
     contact = $(this).val();
-    validateContact(contact);
+    validateContact(contact, contactBool);
     // updatesignUpButton(firstName, lastName, fatherName, cnic, contact, email, password, confirmPassword, matchPassword, lengthPassword, validateEmail, validateContact, validateCNIC)
   })
 
@@ -51,7 +64,7 @@ $(document).ready(function(){
     password = $(this).val();
     box  = $(this);
     alert = box.next();
-    lengthPassword (password, box, alert);
+    lengthPassword (password, box, alert, passwordBool);
     // updatesignUpButton(firstName, lastName, fatherName, cnic, contact, email, password, confirmPassword, matchPassword, lengthPassword, validateEmail, validateContact, validateCNIC)
   })
 
@@ -59,129 +72,192 @@ $(document).ready(function(){
     confirmPassword = $(this).val();
     box  = $(this);
     alert = box.next();
-    matchPassword (confirmPassword, password, box, alert);
+    matchPassword (confirmPassword, password, box, alert, confirmPasswordBool);
     // updatesignUpButton(firstName, lastName, fatherName, cnic, contact, email, password, confirmPassword, matchPassword, lengthPassword, validateEmail, validateContact, validateCNIC)
   })
 
   $("#sign-up").click(function(event) {
     event.preventDefault(); // Prevent the form from submitting
-    updatesignUpButton(firstName, lastName, fatherName, cnic, contact, email, password, confirmPassword, matchPassword, lengthPassword, validateEmail, validateContact, validateCNIC)
-    })
+    // firstName === "" || lastName === "" || fatherName === "" || cnic === "" || contact === "" || email === "" || password === "" || confirmPassword === "" || !matchPassword() || !lengthPassword() || !validateEmail() || !validateContact() || !validateCNIC() 
+    var successNote = $("#h1").next();
+    // updatesignUpButton(firstName, lastName, fatherName, cnic, contact, email, password, confirmPassword, matchPassword, lengthPassword, validateEmail, validateContact, validateCNIC)
+    if (firstNameBool && lastNameBool && fatherNameBool && emailBool && contactBool && cnicBool && passwordBool && confirmPasswordBool) {
+
+      successNote.text("Please fill the credentials as prescribed");
+      successNote.css("border","1px solid white");
+      successNote.css("padding","3px");
+      successNote.css("backgroundColor","red"); 
+
+    } 
+    
+    else {
+
+      successNote.text("you are successfully signed up");
+      successNote.css("border","1px solid white");
+      successNote.css("padding","3px");
+      successNote.css("backgroundColor","#10B981");  
+  
+      // Add a delay before reloading the page
+      setTimeout(function() {
+        location.reload(); // Reload the page
+      }, 1000); // 1000 milliseconds = 1 seconds
+    }
+  
+  })
 
 
 
 
 
 /////////////////////////////////////////////////////// functions //
-function lengthPassword(password, box, alert) {
+function lengthPassword(password, box, alert, pBool) {
 
   if (password === "") {
     alert.text("* this field is mandatory.");
     box.css( "border", "4px double red" );
-    return false;
+    pBool = false; 
+    return pBool;
   } else if (password.length < 6) {
       alert.text("Password must be at least 6 characters.");
       box.css( "border", "4px double red" );
-      return false;
+      pBool = false; 
+      return pBool;
   } else if (password.length > 10) {
       alert.text("Password cannot be longer than 10 characters.");
       box.css( "border", "4px double red" );
-      return false;
+      pBool = false; 
+      return pBool;
   } else if (password.length >= 6 && password.length <= 10) {
-    alert.empty();
-    box.css( "border", "4px double green" );
-    return true;
+      alert.empty();
+      box.css( "border", "4px double green" );
+      pBool = true; 
+      return pBool;
   }
 }
 /////////////////////////////////////////////////////////////////////////
-function matchPassword(confirmPassword, password, box, alert) {
+function matchPassword(confirmPassword, password, box, alert, cpBool ) {
   if (confirmPassword === "") {
     alert.text("* this field is mandatory.");
     box.css( "border", "4px double red" );
-    return false;
+    cpBool = false; 
+    return cpBool;
   }
   else if (password !== confirmPassword || confirmPassword !== password ) {
       alert.text("Passwords do not match.");
       box.css( "border", "4px double red" );
-      return false;
+      cpBool = false; 
+      return cpBool;
   }
   else if (password === confirmPassword || confirmPassword === password) {
     box.css( "border", "4px double green" );
     alert.empty();
-    return true;
+    cpBool = true; 
+    return cpBool;
   } 
 }
 ////////////////////////////////////////////////
 
-function validateEmail(inputText) {
+function validateEmail(inputText, mBool) {
 
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(inputText.match(mailformat)) {
+
+  if( inputText === "") {
+    $("#emailAddr").css( "border", "4px double red" );
+    $("#emailAddr").next().text("* this field is mandatory");
+    mBool = false; 
+    return mBool;
+  }
+  else if(inputText.match(mailformat)) {
 
     $("#emailAddr").css( "border", "4px double green" );
     $("#emailAddr").next().empty();
-    return true;
+    mBool = true; 
+    return mBool;
   }
 
   else {
     $("#emailAddr").css( "border", "4px double red" );
     $("#emailAddr").next().text("* You have entered an invalid email address!");
-    return false;
+    mBool = false; 
+    return mBool;
   }
 }
 
 ////////////////////////////////////////////////////
 
-function validateCNIC(inputNum) {
+function validateCNIC(inputNum, cnBool ) {
   var cnicFormat = /^[0-9]{5}-[0-9]{7}-[0-9]$/;
-  if(inputNum.match(cnicFormat)) {
+
+  if(inputNum === "") {
+    $("#cnic").css( "border", "4px double red" );
+    $("#cnic").next().text("* this field is mandatory");
+    cnBool = false; 
+    return cnBool;
+  }
+  else if(inputNum.match(cnicFormat)) {
 
     $("#cnic").css( "border", "4px double green" );
     $("#cnic").next().empty();
-    return true;
+    cnBool = true; 
+    return cnBool;
   }
 
   else {
     $("#cnic").css( "border", "4px double red" );
     $("#cnic").next().text("* You have entered an invalid CNIC number!");
-    return false;
+    cnBool = false; 
+    return cnBool;
   }
 }
 ////////////////////////////////
 
-function validateContact(inputContact) {
+function validateContact(inputContact, coBool) {
   var contactFormat = /^[0][3][0-9]{9}$/;
-  if(inputContact.match(contactFormat)) {
+
+  if (inputContact === "") {
+    $("#contact").css( "border", "4px double red" );
+    $("#contact").next().text("* this field is mandatory");
+    coBool = false; 
+    return coBool;
+  }
+  else if(inputContact.match(contactFormat)) {
 
     $("#contact").css( "border", "4px double green" );
     $("#contact").next().empty();
-    return true;
+    coBool = true; 
+    return coBool;
   }
 
   else {
     $("#contact").css( "border", "4px double red" );
     $("#contact").next().text("* You have entered an invalid Contact Number!");
-    return false;
+    coBool = false; 
+    return coBool;
   }
 }
 ////////////////////////////////
 
-function validateName (name, box, alert) {
-    if(name == "") {
+function validateName (name, box, alert, inputBool) {
+    if(name === "") {
       alert.text("* this field is mandatory.");
       box.css( "border", "3px double red" );
+      inputBool = false; 
+      return inputBool
     }
     else if (name.length > 0 && name.length < 4 ) {
       for (var i = 0; i < name.length; i++) {
         if(name[i] === " ") {
           alert.text("* no spaces allowed.");
           box.css( "border", "3px double red" );
-          return;
+          inputBool = false; 
+          return inputBool;
         }
       
         else {
           alert.text("* the name is too short.");
           box.css( "border", "3px double yellow" );
+          inputBool = false; 
+          return inputBool; 
         }
       }
       
@@ -192,12 +268,15 @@ function validateName (name, box, alert) {
         if(name[i] === " ") {
           alert.text("* no spaces allowed.");
           box.css( "border", "3px double red" );
-          return;
+          inputBool = false; 
+          return inputBool; 
         }
       
         else {
           alert.empty();
           box.css( "border", "3px double green" );
+          inputBool = true; 
+          return inputBool; 
         }
       }
     }
